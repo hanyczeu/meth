c3f9c5095484ae1ce084cee0aa584b5fa72eb80c
HanyCZeu <74603656+hanyczeu@users.noreply.github.com>
2022-08-01 08:07:15 +0200

    Update client.lua

--- qb-meth/client/client.lua
+++ qb-meth/client/client.lua
@@ -1,141 +1,126 @@
-QBCore = nil
 local QBCore = exports['qb-core']:GetCoreObject()
+
 isLoggedIn = true
 
 local menuOpen = false
 local wasOpen = false
+local spawnedMeth = 0
+local methPlants = {}
 
-Citizen.CreateThread(function() 
-    while true do
-        Citizen.Wait(10)
-        if QBCore == nil then
-            TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
-            Citizen.Wait(200)
-        end
-    end
-end)
+local isPickingUp, isProcessing, isProcessing2 = false, false, false
 
-local spawnedWeed = 0
-local weedPlants = {}
-
-local isPickingUp, isProcessing, isProcessing3 = false, false, false
-
-RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
-AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
-	CheckCoords3()
-	Citizen.Wait(1000)
+RegisterNetEvent("QBCore:Client:OnPlayerLoaded", function()
+	CheckCoords()
+	Wait(1000)
 	local coords = GetEntityCoords(PlayerPedId())
-	if GetDistanceBetweenCoords(coords, Config.CircleZones.PickupMeth.coords, true) < 1000 then
-		SpawnweedPlants3()
+	if GetDistanceBetweenCoords(coords, Config.CircleZones.MethField.coords, true) < 1000 then
+		SpawnMethPlants()
 	end
 end)
 
-function CheckCoords3()
-	Citizen.CreateThread(function()
+function CheckCoords()
+	CreateThread(function()
 		while true do
 			local coords = GetEntityCoords(PlayerPedId())
-			if GetDistanceBetweenCoords(coords, Config.CircleZones.PickupMeth.coords, true) < 1000 then
-				SpawnweedPlants3()
+			if GetDistanceBetweenCoords(coords, Config.CircleZones.MethField.coords, true) < 1000 then
+				SpawnMethPlants()
 			end
-			Citizen.Wait(1 * 60000)
+			Wait(1 * 60000)
 		end
 	end)
 end
 
 AddEventHandler('onResourceStart', function(resource)
 	if resource == GetCurrentResourceName() then
-		CheckCoords3()
+		CheckCoords()
 	end
 end)
-Citizen.CreateThread(function()--weed
+
+CreateThread(function()--Meth
 	while true do
-		Citizen.Wait(0)
+		Wait(10)
 
 		local playerPed = PlayerPedId()
 		local coords = GetEntityCoords(playerPed)
 		local nearbyObject, nearbyID
-		
-		
-		for i=1, #weedPlants, 1 do
-			if GetDistanceBetweenCoords(coords, GetEntityCoords(weedPlants[i]), false) < 1 then
-				nearbyObject, nearbyID = weedPlants[i], i
+
+
+		for i=1, #methPlants, 1 do
+			if GetDistanceBetweenCoords(coords, GetEntityCoords(methPlants[i]), false) < 1 then
+				nearbyObject, nearbyID = methPlants[i], i
 			end
 		end
 
 		if nearbyObject and IsPedOnFoot(playerPed) then
 
 			if not isPickingUp then
-				QBCore.Functions.Draw2DText(0.5, 0.88, 'Press ~g~[E]~w~ to Pick up Some Chemicals ', 0.4)
+				QBCore.Functions.Draw2DText(0.5, 0.88, 'Press ~g~[E]~w~ to pickup Meth', 0.4)
 			end
 
 			if IsControlJustReleased(0, 38) and not isPickingUp then
 				isPickingUp = true
-				TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, false)
-				--PROP_BARREL_03A animazione
-				--prop_cs_cardbox_01 oggetto di spawn  prop_plant_01a
-				QBCore.Functions.Progressbar("search_register", "Picking Up Some Chemicals..", 6500, false, true, {
+				TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_PARKING_METER', 0, false)
+				QBCore.Functions.Progressbar("search_register", "Picking up Meth..", 3000, false, true, {
 					disableMovement = true,
 					disableCarMovement = true,
 					disableMouse = false,
 					disableCombat = true,
 					disableInventory = true,
 				}, {}, {}, {}, function() -- Done
-					ClearPedTasks(GetPlayerPed(-1))
+					ClearPedTasks(PlayerPedId())
 					QBCore.Functions.DeleteObject(nearbyObject)
 
-					table.remove(weedPlants, nearbyID)
-					spawnedWeed = spawnedWeed - 1
+					table.remove(methPlants, nearbyID)
+					spawnedMeth = spawnedMeth - 1
 
 					TriggerServerEvent('qb-meth:pickedUpMeth')
 				end, function()
-					ClearPedTasks(GetPlayerPed(-1))
-				end) -- Cancel
+					ClearPedTasks(PlayerPedId())
+				end)
 
 				isPickingUp = false
 			end
 		else
-			Citizen.Wait(500)
+			Wait(500)
 		end
 	end
 end)
 
-
-AddEventHandler('onResourceStop', function(resource) --weedPlants
+AddEventHandler('onResourceStop', function(resource)
 	if resource == GetCurrentResourceName() then
-		for k, v in pairs(weedPlants) do
-			QBCore.Functions.DeleteObject(v)
+		for k, v in pairs(methPlants) do
+			QBCore.Functions.DeleteObject(nearbyObject)
 		end
 	end
 end)
-function SpawnweedPlants3() --This spawns in the Weed plants, 
-	while spawnedWeed < 20 do
-		Citizen.Wait(1)
-		local weedCoords = GenerateWeedCoords3()
---prop_barrel_01a  prop_plant_01a prop_barrel_01a
-		QBCore.Functions.SpawnLocalObject('prop_barrel_01a', weedCoords, function(obj) --- change this prop to whatever plant you are trying to use 
+function SpawnMethPlants()
+	while spawnedMeth < 20 do
+		Wait(1)
+		local methCoords = GenerateMethCoords()
+
+		QBCore.Functions.SpawnLocalObject('prop_barrel_01a', methCoords, function(obj)
 			PlaceObjectOnGroundProperly(obj)
 			FreezeEntityPosition(obj, true)
-			
 
-			table.insert(weedPlants, obj)
-			spawnedWeed = spawnedWeed + 1
+
+			table.insert(methPlants, obj)
+			spawnedMeth = spawnedMeth + 1
 		end)
 	end
-	Citizen.Wait(45 * 60000)
+	Wait(45 * 60000)
 end
 
-
-function ValidateWeedCoord(plantCoord) --This is a simple validation checker
-	if spawnedWeed > 0 then
+function ValidateMethCoord(plantCoord)
+	if spawnedMeth > 0 then
 		local validate = true
 
-		for k, v in pairs(weedPlants) do
+		for k, v in pairs(methPlants) do
 			if GetDistanceBetweenCoords(plantCoord, GetEntityCoords(v), true) < 5 then
 				validate = false
 			end
 		end
 
-		if GetDistanceBetweenCoords(plantCoord, Config.CircleZones.PickupMeth.coords, false) > 50 then
+		if GetDistanceBetweenCoords(plantCoord, Config.CircleZones.MethField.coords, false) > 50 then
 			validate = false
 		end
 
@@ -145,33 +130,33 @@ function ValidateWeedCoord(plantCoord) --This is a simple validation checker
 	end
 end
 
-function GenerateWeedCoords3() --This spawns the weed plants at the designated location
+function GenerateMethCoords()
 	while true do
-		Citizen.Wait(1)
+		Wait(1)
 
-		local weedCoordX, weedCoordY
+		local methCoordX, methCoordY
 
 		math.randomseed(GetGameTimer())
 		local modX = math.random(-10, 10)
 
-		Citizen.Wait(100)
+		Wait(100)
 
 		math.randomseed(GetGameTimer())
 		local modY = math.random(-10, 10)
 
-		weedCoordX = Config.CircleZones.PickupMeth.coords.x + modX
-		weedCoordY = Config.CircleZones.PickupMeth.coords.y + modY
+		methCoordX = Config.CircleZones.MethField.coords.x + modX
+		methCoordY = Config.CircleZones.MethField.coords.y + modY
 
-		local coordZ = GetCoordZWeed(weedCoordX, weedCoordY)
-		local coord = vector3(weedCoordX, weedCoordY, coordZ)
+		local coordZ = GetCoordZMeth(methCoordX, methCoordY)
+		local coord = vector3(methCoordX, methCoordY, coordZ)
 
-		if ValidateWeedCoord(coord) then
+		if ValidateMethCoord(coord) then
 			return coord
 		end
 	end
 end
 
-function GetCoordZWeed(x, y) ---- Set the coordinates relative to the heights near where you want the circle spawning
+function GetCoordZMeth(x, y)
 	local groundCheckHeights = { 31.0, 32.0, 33.0, 34.0, 35.0, 36.0, 37.0, 38.0, 39.0, 40.0, 50.0 }
 
 	for i, height in ipairs(groundCheckHeights) do
@@ -185,162 +170,179 @@ function GetCoordZWeed(x, y) ---- Set the coordinates relative to the heights ne
 	return 31.85
 end
 
-Citizen.CreateThread(function() --- check that makes sure you have the materials needed to process
+CreateThread(function()
 	while QBCore == nil do
-		Citizen.Wait(200)
+		Wait(200)
 	end
 	while true do
-		Citizen.Wait(0)
+		Wait(10)
 		local playerPed = PlayerPedId()
 		local coords = GetEntityCoords(playerPed)
 
-		if GetDistanceBetweenCoords(coords, Config.CircleZones.ProcessMeth.coords, true) < 1 then
-			DrawMarker(2, Config.CircleZones.ProcessMeth.coords.x, Config.CircleZones.ProcessMeth.coords.y, Config.CircleZones.ProcessMeth.coords.z - 0.2 , 0, 0, 0, 0, 0, 0, 0.3, 0.2, 0.15, 255, 0, 0, 100, 0, 0, 0, true, 0, 0, 0)
+		if GetDistanceBetweenCoords(coords, Config.CircleZones.MethProcessing.coords, true) < 5 then
+			DrawMarker(2, Config.CircleZones.MethProcessing.coords.x, Config.CircleZones.MethProcessing.coords.y, Config.CircleZones.MethProcessing.coords.z - 0.2 , 0, 0, 0, 0, 0, 0, 0.3, 0.2, 0.15, 255, 0, 0, 100, 0, 0, 0, true, 0, 0, 0)
 
-			
-			if not isProcessing then
-				QBCore.Functions.DrawText3D(Config.CircleZones.ProcessMeth.coords.x, Config.CircleZones.ProcessMeth.coords.y, Config.CircleZones.ProcessMeth.coords.z, 'Press ~g~[ E ]~w~ to Process Meth')
+
+			if not isProcessing and GetDistanceBetweenCoords(coords, Config.CircleZones.MethProcessing.coords, true) <1 then
+				QBCore.Functions.DrawText3D(Config.CircleZones.MethProcessing.coords.x, Config.CircleZones.MethProcessing.coords.y, Config.CircleZones.MethProcessing.coords.z, 'Press ~g~[E]~w~ to Process')
 			end
 
 			if IsControlJustReleased(0, 38) and not isProcessing then
+				local hasBag = false
 				local s1 = false
-				local hasWeed = false
+				local hasMeth = false
+				local s2 = false
 
 				QBCore.Functions.TriggerCallback('QBCore:HasItem', function(result)
-					hasWeed = result
+					hasMeth = result
 					s1 = true
-				end, 'cocaine_leaf')
-				
+				end, 'chemicals')
+
 				while(not s1) do
-					Citizen.Wait(100)
+					Wait(100)
+				end
+				Wait(100)
+				QBCore.Functions.TriggerCallback('QBCore:HasItem', function(result)
+					hasBag = result
+					s2 = true
+				end, 'plastic_baggie')
+
+				while(not s2) do
+					Wait(100)
 				end
 
-				if (hasWeed) then
-					Processweed3()
-				elseif (hasWeed) then
-					QBCore.Functions.Notify('You dont have Acid Bottles.', 'error')
+				if (hasMeth and hasBag) then
+					Processmeth()
+				elseif (hasMeth) then
+					QBCore.Functions.Notify('You dont have enough plastic bags.', 'error')
+				elseif (hasBag) then
+					QBCore.Functions.Notify('You dont have enough chemicals.', 'error')
+				else
+					QBCore.Functions.Notify('You dont have enough chemicals and plastic bags.', 'error')
 				end
 			end
 		else
-			Citizen.Wait(500)
+			Wait(500)
 		end
 	end
 end)
 
-
-function Processweed3()  -- simple animations to loop while process is taking place
+function Processmeth()
 	isProcessing = true
 	local playerPed = PlayerPedId()
 
-	--
 	TaskStartScenarioInPlace(playerPed, "PROP_HUMAN_PARKING_METER", 0, true)
 	SetEntityHeading(PlayerPedId(), 108.06254)
 
-	QBCore.Functions.Progressbar("search_register", "Packing Meth..", 15000, false, true, {
+	QBCore.Functions.Progressbar("search_register", "Trying to Process..", 30000, false, true, {
 		disableMovement = true,
 		disableCarMovement = true,
 		disableMouse = false,
 		disableCombat = true,
 		disableInventory = true,
 	}, {}, {}, {}, function()
-	 TriggerServerEvent('qb-meth:processmeth') -- Done
+	 TriggerServerEvent('qb-meth:processmeth')
 
-		local timeLeft = Config.Delays.ProcessMeth / 1000
+		local timeLeft = Config.Delays.MethProcessing / 1000
 
 		while timeLeft > 0 do
-			Citizen.Wait(1000)
+			Wait(1000)
 			timeLeft = timeLeft - 1
 
-			if GetDistanceBetweenCoords(GetEntityCoords(playerPed), Config.CircleZones.ProcessMeth.coords, false) > 4 then
-				TriggerServerEvent('qb-meth:cancelProcessing3')
+			if GetDistanceBetweenCoords(GetEntityCoords(playerPed), Config.CircleZones.MethProcessing.coords, false) > 4 then
+				TriggerServerEvent('qb-meth:cancelProcessing')
 				break
 			end
 		end
-		ClearPedTasks(GetPlayerPed(-1))
+		ClearPedTasks(PlayerPedId())
 	end, function()
-		ClearPedTasks(GetPlayerPed(-1))
+		ClearPedTasks(PlayerPedId())
 	end) -- Cancel
-		
-	
+
 	isProcessing = false
 end
 
-
-Citizen.CreateThread(function() --- check that makes sure you have the materials needed to process
+CreateThread(function()
 	while QBCore == nil do
-		Citizen.Wait(200)
+		Wait(200)
 	end
 	while true do
-		Citizen.Wait(0)
+		Wait(10)
 		local playerPed = PlayerPedId()
 		local coords = GetEntityCoords(playerPed)
 
-		if GetDistanceBetweenCoords(coords, Config.CircleZones.DrugDealer.coords, true) < 1 then
+		if GetDistanceBetweenCoords(coords, Config.CircleZones.DrugDealer.coords, true) < 5 then
 			DrawMarker(2, Config.CircleZones.DrugDealer.coords.x, Config.CircleZones.DrugDealer.coords.y, Config.CircleZones.DrugDealer.coords.z - 0.2 , 0, 0, 0, 0, 0, 0, 0.3, 0.2, 0.15, 255, 0, 0, 100, 0, 0, 0, true, 0, 0, 0)
 
-			
-			if not isProcessing3 then
-				QBCore.Functions.DrawText3D(Config.CircleZones.DrugDealer.coords.x, Config.CircleZones.DrugDealer.coords.y, Config.CircleZones.DrugDealer.coords.z, 'Press ~g~[ E ]~w~ to Sell')
+
+			if not isProcessing2 and GetDistanceBetweenCoords(coords, Config.CircleZones.DrugDealer.coords, true) <1 then
+				QBCore.Functions.DrawText3D(Config.CircleZones.DrugDealer.coords.x, Config.CircleZones.DrugDealer.coords.y, Config.CircleZones.DrugDealer.coords.z, 'Press ~g~[E]~w~ to Sell')
 			end
 
-			if IsControlJustReleased(0, 38) and not isProcessing3 then
-	            local s1 = false
-				local hasWeed = false
+			if IsControlJustReleased(0, 38) and not isProcessing2 then
+				local hasMeth2 = false
+				local hasBag2 = false
+				local s3 = false
 
 				QBCore.Functions.TriggerCallback('QBCore:HasItem', function(result)
-					hasWeed = result
-					s1 = true
+					hasMeth2 = result
+					hasBag2 = result
+					s3 = true
+
 				end, 'meth')
-				
-				while(not s1) do
-					Citizen.Wait(100)
+
+				while(not s3) do
+					Wait(100)
 				end
 
-				if (hasWeed) then
-					SellDrug3()
-				elseif (hasWeed) then
-					QBCore.Functions.Notify('You dont have Meth.', 'error')
+
+				if (hasMeth2) then
+					SellDrug()
+				elseif (hasMeth2) then
+					QBCore.Functions.Notify('You dont have enough plastic bags.', 'error')
+				elseif (hasBag2) then
+					QBCore.Functions.Notify('You dont have enough chemicals.', 'error')
+				else
+					QBCore.Functions.Notify('You dont have enough meth bags to sell.', 'error')
 				end
 			end
 		else
-			Citizen.Wait(500)
+			Wait(500)
 		end
 	end
 end)
 
-function SellDrug3()  -- simple animations to loop while process is taking place
-	isProcessing3 = true
+function SellDrug()
+	isProcessing2 = true
 	local playerPed = PlayerPedId()
 
 	--
-	TaskStartScenarioInPlace(playerPed, "CODE_HUMAN_MEDIC_TEND_TO_DEAD", 0, true)
+	TaskStartScenarioInPlace(playerPed, "PROP_HUMAN_PARKING_METER", 0, true)
 	SetEntityHeading(PlayerPedId(), 108.06254)
 
-	QBCore.Functions.Progressbar("search_register", "Selling Meth..", 15000, false, true, {
+	QBCore.Functions.Progressbar("search_register", "Trying to Sell..", 1500, false, true, {
 		disableMovement = true,
 		disableCarMovement = true,
 		disableMouse = false,
 		disableCombat = true,
-		disableInventory = false,
+		disableInventory = true,
 	}, {}, {}, {}, function()
-	 TriggerServerEvent('qb-meth:selld3') -- Done
+	 TriggerServerEvent('qb-meth:selld')
 
-		local timeLeft = Config.Delays.ProcessMeth / 1000
+		local timeLeft = Config.Delays.MethProcessing / 1000
 
 		while timeLeft > 0 do
-			Citizen.Wait(500)
+			Wait(500)
 			timeLeft = timeLeft - 1
 
-			if GetDistanceBetweenCoords(GetEntityCoords(playerPed), Config.CircleZones.ProcessMeth.coords, false) > 4 then
-				--TriggerServerEvent('qb-meth:cancelProcessing3')
+			if GetDistanceBetweenCoords(GetEntityCoords(playerPed), Config.CircleZones.MethProcessing.coords, false) > 4 then
 				break
 			end
 		end
-		ClearPedTasks(GetPlayerPed(-1))
+		ClearPedTasks(PlayerPedId())
 	end, function()
-		ClearPedTasks(GetPlayerPed(-1))
+		ClearPedTasks(PlayerPedId())
 	end) -- Cancel
-		
-	
-	isProcessing3 = false
+
+	isProcessing2 = false
 end
